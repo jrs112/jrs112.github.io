9c250d4f76ae4146814aca3825889bf924e070a9
john sander <john sander>
2017-04-18 14:28:52 -0400

    update contact me form

--- assets/javascript/submission.js
+++ assets/javascript/submission.js
@@ -5,14 +5,17 @@ myform.submit(function(event){
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
   var template_id = "portfolio_submission";
-   var template_idTwo = "portfolio_response";
+  var template_idTwo = "portfolio_response";
 
   myform.find("button").text("Sending...");
   emailjs.sendForm(service_id,template_id,"myform")
   emailjs.sendForm(service_id,template_idTwo,"myform")
     .then(function(){
         alert("Sent!");
-       myform.find("button").text("Send");
+        myform.find("button").text("Send");
+        $("#userName").val("");
+        $("#userEmail").val("");
+        $("#userMessage").val("");
     }, function(err) {
        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
        myform.find("button").text("Send");
